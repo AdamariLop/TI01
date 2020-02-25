68bda43d2ed63cfd791e5be661d2bd32c4c1fd30
adama <adamarilopez04@gmail.com>
2020-02-25 12:36:14 -0600

    Cambios en el archivo App.js

--- App.js
+++ App.js
@@ -1,2 +1,10 @@
 import React, {Component} from 'React';
 import { Text } from 'react-native';
+
+class Prueba extends Component{
+    return(){
+    <Text>Hola mundo</Text>
+    }
+}
+
+export default Prueba;
